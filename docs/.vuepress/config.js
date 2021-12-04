3cf2407bbb70fdd53693e70fcd1e11310f6b8647
Mason <ab300819@163.com>
2021-12-04 16:13:47 +0800

    配置主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,19 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
 module.exports = {
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
+    title: "Mason's blog",
     description: '这是我的第一个 VuePress 站点',
     theme:'vdoing',
     themeConfig: {
         logo: 'https://vuejs.org/images/logo.png',
     },
+    markdown:{
+        lineNumbers: true,
+    },
+    head,
+    plugins,
+    themeConfig,
 }
